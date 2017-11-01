10a4e1463d77e33c2797d87a5b63bb517583b5e8
JanPC <vegita321@gmail.com>
2017-11-01 13:31:03 +0100

    fixed bug with filenames beeing wrong on windows

--- backend/src/controllers/api.ts
+++ backend/src/controllers/api.ts
@@ -5,7 +5,6 @@ import * as path from 'path';
 import {Response, Request, NextFunction} from 'express';
 import {config} from '../config';
 
-
 export let getAllFiles = (req: Request, res: Response) => {
 	res.json({
 		images: listAllFiles(path.resolve(config.pathToCrosscode, 'media/'), [], 'png'),
@@ -20,7 +19,8 @@ function listAllFiles(dir: string, filelist, ending: string): string[] {
 		if (fs.statSync(path.resolve(dir, file)).isDirectory()) {
 			filelist = listAllFiles(path.resolve(dir, file), filelist, ending);
 		} else if (!ending || file.toLowerCase().endsWith(ending.toLowerCase())) {
-			filelist.push(path.resolve(dir, file).split(config.pathToCrosscode)[1]);
+			const normalized = path.resolve(dir, file).split(path.normalize(config.pathToCrosscode))[1];
+			filelist.push(normalized.split('\\').join('/'));
 		}
 	});
 	return filelist;
