f922c77de2f5037109b4b1c73af56edc7d8175b6
JanPC <vegita321@gmail.com>
2018-11-11 11:32:28 +0100

    moved json to assets and fixed style issues

--- webapp/src/app/shared/dialogs/map-settings/map-settings.component.ts
+++ webapp/src/app/shared/dialogs/map-settings/map-settings.component.ts
@@ -3,6 +3,7 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 import {CrossCodeMap} from '../../interfaces/cross-code-map';
 import {MapLoaderService} from '../../map-loader.service';
 import {CCMap} from '../../phaser/tilemap/cc-map';
+import * as mapSettingsjson from '../../../../assets/map-settings.json';
 
 @Component({
 	selector: 'app-map-settings',
@@ -10,24 +11,18 @@ import {CCMap} from '../../phaser/tilemap/cc-map';
 	styleUrls: ['./map-settings.component.scss']
 })
 export class MapSettingsComponent {
-
+	
 	private tileMap: CCMap;
-	weatherTypes = require('./map-settings.json').weather;
-	saveModeTypes = require('./map-settings.json').saveModeTypes;
-	bgm = require('./map-settings.json').bgm;
-	mapSounds = require('./map-settings.json').mapSounds;
-	mapStyles = require('./map-settings.json').mapStyles;
-	areas = require('./map-settings.json').areas;
-	npcRunnerTypes = require('./map-settings.json').npcRunnerTypes;
+	mapSettings = mapSettingsjson.default;
 	settings: CrossCodeMap = <any>{
 		levels: [{height: -32}, {height: 0}, {height: 32}, {height: 64}],
 		attributes: {},
 	};
-
+	
 	constructor(private loader: MapLoaderService, public ref: MatDialogRef<MapSettingsComponent>) {
 		this.tileMap = loader.tileMap.getValue();
 		const tileMap = this.tileMap;
-
+		
 		if (!tileMap) {
 			return;
 		}
@@ -38,18 +33,18 @@ export class MapSettingsComponent {
 		settings.masterLevel = tileMap.masterLevel;
 		settings.attributes = tileMap.attributes;
 	}
-
+	
 	update() {
 		// TODO: add validation
 		const settings = this.settings;
 		const tileMap = this.tileMap;
-
+		
 		tileMap.levels = settings.levels;
 		tileMap.masterLevel = settings.masterLevel;
 		tileMap.attributes = settings.attributes;
-
+		
 		tileMap.resize(settings.mapWidth, settings.mapHeight);
-
+		
 		this.ref.close();
 	}
 }
