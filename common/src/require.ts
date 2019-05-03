4e9af32ec12e3498396c60c1bc6715fe9f4fcedf
ac2pic <ac2pic@gmail.com>
2019-05-03 18:01:27 -0500

    Revert git glitch
    This reverts commit 3f05f738eae59b6b3b6a2ddacbf02622ba04d617.

--- common/src/require.ts
+++ common/src/require.ts
@@ -1,15 +1,14 @@
 let result: NodeRequireFunction | (() => null);
 
-if (typeof window !== 'undefined' && 
-	typeof navigator !== 'undefined' &&
-	navigator.userAgent.indexOf('Electron') > -1) { 
-	
-	const electron = window['require']('electron');
-	// Use electron require
-	result = electron.remote.require;
-} else if (typeof process !== 'undefined' && process.versions.node) {
-	// Regular nodejs
-	result = module['require'];
+if (!!module['require']) {
+    try {
+        const electron = window['require']('electron');
+        // Use electron require
+        result = electron.remote.require;
+    } catch {
+        // Regular nodejs
+        result = module['require'];
+    }
 } else {
     // Not a supported plattform. Return dummy
     result = () => null;
