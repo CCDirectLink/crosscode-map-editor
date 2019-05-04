3731ab2c324d609986707cd900dad3a00af605d3
ac2pic <ac2pic@gmail.com>
2019-05-04 12:53:25 -0500

    Now only need to specify module name.

--- backend/src/controllers/api.ts
+++ backend/src/controllers/api.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import {Response, Request, NextFunction} from 'express';
-import {listAllFiles, config} from 'cc-map-editor-common/dist/main';
+import {listAllFiles, config} from 'cc-map-editor-common';
 export const getAllFiles = (req: Request, res: Response) => {
 	res.json({
 		images: listAllFiles(path.resolve(config.pathToCrosscode, 'media/'), [], 'png'),
--- webapp/src/app/services/http-client.service.ts
+++ webapp/src/app/services/http-client.service.ts
@@ -4,7 +4,7 @@ import {Observable} from 'rxjs';
 import {Globals} from '../shared/globals';
 import {Remote, Dialog} from 'electron';
 import { FileInfos } from '../models/file-infos';
-import { listAllFiles } from 'cc-map-editor-common/dist/main';
+import { listAllFiles } from 'cc-map-editor-common';
 
 @Injectable()
 export class HttpClientService {
