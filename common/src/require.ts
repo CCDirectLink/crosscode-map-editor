ddccc5856bad49d2708f97100fb406b2d0dc2449
2767mr <2767mr@gmx.at>
2019-05-09 19:42:30 +0200

    Improved require.ts

--- common/src/require.ts
+++ common/src/require.ts
@@ -1,4 +1,4 @@
-let result: NodeRequireFunction | (() => null);
+let result: NodeRequire;
 
 if (typeof window !== 'undefined' && 
 	typeof navigator !== 'undefined' &&
@@ -6,13 +6,20 @@ if (typeof window !== 'undefined' &&
 
 	const electron = window['require']('electron');
 	// Use electron require
-	result = electron.remote.require;
+	result = electron.remote.require as NodeRequire;
 } else if (typeof process !== 'undefined' && !!module['require']) {
 	// Regular nodejs
-	result = module['require'];
+	result = require;
 } else {
-	// Not a supported plattform. Return dummy
-	result = () => null;
+	// Not a supported plattform. Return dummy, do not fail on execute of the lambda.
+	result = (() => new Proxy({}, {
+        get() {
+            throw new Error('This platform is not supported');
+        },
+        set() {
+            throw new Error('This platform is not supported');
+        }
+    })) as unknown as NodeRequire;
 }
 
 export const requireLocal = result;
