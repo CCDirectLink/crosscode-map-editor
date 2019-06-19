80b0baed839c7df5f80f44009ed4c6fa0d09692c
2767mr <2767mr@gmx.at>
2019-06-19 14:29:27 +0200

    Updated mousewheel callback

--- webapp/src/typings.d.ts
+++ webapp/src/typings.d.ts
@@ -2,3 +2,9 @@ declare module '*.json' {
 	const value: any;
 	export default value;
 }
+
+declare namespace Phaser {
+	interface Input {
+		mouseWheel: Phaser.MouseWheel; //Workaround for missing typings
+	}
+}
