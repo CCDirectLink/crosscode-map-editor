b8c9c5251043bff97b6768146b1b64f30eb873ed
cramerL <vegita321@gmail.com>
2019-07-13 20:46:47 +0200

    added spacing back to overlay panel comment

--- webapp/src/app/shared/overlay/overlay-panel/overlay-panel.component.ts
+++ webapp/src/app/shared/overlay/overlay-panel/overlay-panel.component.ts
@@ -1,22 +1,21 @@
 import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 
-
 /**
  * Used to display an overlay with the basic styles (dragable toolbar, scrollable content and buttons at the bottom)
  * example usage:
  * ```html
- <cc-overlay-panel title="Panel Title">
- <ng-container ngProjectAs="toolbar">
- custom toolbar
- </ng-container>
- <ng-container ngProjectAs="content">
- content...
- </ng-container>
- <ng-container ngProjectAs="actions">
- <button mat-stroked-button (click)="cancel()">Cancel</button>
- <button mat-stroked-button (click)="update()">Update</button>
- </ng-container>
- </cc-overlay-panel>
+ * <cc-overlay-panel title="Panel Title">
+ *     <ng-container ngProjectAs="toolbar">
+ *         custom toolbar
+ *     </ng-container>
+ *     <ng-container ngProjectAs="content">
+ *         content...
+ *     </ng-container>
+ *     <ng-container ngProjectAs="actions">
+ *         <button mat-stroked-button (click)="cancel()">Cancel</button>
+ *         <button mat-stroked-button (click)="update()">Update</button>
+ *     </ng-container>
+ * </cc-overlay-panel>
  * ```
  */
 @Component({
