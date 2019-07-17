9bfa2bdaca739c39985b41ff082c8fe579ab2e82
ac2pic <ac2pic@gmail.com>
2019-07-16 21:26:22 -0500

    Added label event

---
+++ webapp/src/app/shared/widgets/event-widget/event-registry/label.ts
@@ -0,0 +1,33 @@
+import {AbstractEvent, EventType} from './abstract-event';
+
+
+export interface LabelData extends EventType {
+	name: string;
+};
+
+export class Label extends AbstractEvent<LabelData> {
+	private attributes = {
+		name : {
+			type: 'String',
+			description: 'Name to reference this label by.'
+		}
+	}
+	
+	getAttributes() {
+		return this.attributes;
+	}
+
+	update() {
+		this.info = this.combineStrings(
+			this.getTypeString('#4254f5'),
+			this.getPropString('name')
+		);	
+	}
+
+
+	generateNewDataInternal() {
+		return {
+			name: 'NEW LABEL'
+		};
+	}
+}
