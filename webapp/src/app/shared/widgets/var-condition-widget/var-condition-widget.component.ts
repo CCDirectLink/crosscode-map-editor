6646041537a47d2cad724692f28b8d1f068ed4bd
ac2pic <ac2pic@gmail.com>
2019-07-17 12:22:00 -0500

    VarCondition basic widget structure

---
+++ webapp/src/app/shared/widgets/var-condition-widget/var-condition-widget.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-var-condition-widget',
+  templateUrl: './var-condition-widget.component.html',
+  styleUrls: ['./var-condition-widget.component.scss']
+})
+export class VarConditionWidgetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
