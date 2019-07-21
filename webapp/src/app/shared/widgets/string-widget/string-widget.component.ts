4f9bbb84e51ebb8bb626e80513419b0b7662a38c
ac2pic <ac2pic@gmail.com>
2019-07-21 00:13:44 -0500

    Started integrating AbstractExpressionWidget into string

--- webapp/src/app/shared/widgets/string-widget/string-widget.component.ts
+++ webapp/src/app/shared/widgets/string-widget/string-widget.component.ts
@@ -1,28 +1,25 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {AbstractWidget} from '../abstract-widget';
-
+import {AbstractExpressionWidget} from '../abstract-expression-widget/abstract-expression-widget.component';
 @Component({
 	selector: 'app-string-widget',
 	templateUrl: './string-widget.component.html',
 	styleUrls: ['./string-widget.component.scss', '../widget.scss']
 })
-export class StringWidgetComponent extends AbstractWidget implements OnInit {
+export class StringWidgetComponent extends AbstractExpressionWidget implements OnInit {
 	
 	keys: string[] = [];
-	valueType: string = "value";
+
 	constructor() {
 		super();
 	}
 	
 	ngOnInit() {
+		
 		super.ngOnInit();
 		const attr = this.attribute;
 		if (attr && attr.options) {
 			this.keys = Object.keys(attr.options);
 		}
-	}
-
-	onContextMenu() {
-		alert('Context menu clicked.');
 	}
 }
