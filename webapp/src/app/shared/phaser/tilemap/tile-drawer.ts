1638f29f33dad12627e0e468f37a5e03a09ed93f
cramerL <vegita321@gmail.com>
2019-08-08 03:44:14 +0200

    skip autotiles if shift is held down

--- webapp/src/app/shared/phaser/tilemap/tile-drawer.ts
+++ webapp/src/app/shared/phaser/tilemap/tile-drawer.ts
@@ -26,6 +26,7 @@ export class TileDrawer extends BaseObject {
 	
 	private transparentKey!: Phaser.Input.Keyboard.Key;
 	private visibilityKey!: Phaser.Input.Keyboard.Key;
+	private shiftKey!: Phaser.Input.Keyboard.Key;
 	private fillKey!: Phaser.Input.Keyboard.Key;
 	private lastDraw: Point = {x: -1, y: -1};
 	
@@ -37,6 +38,7 @@ export class TileDrawer extends BaseObject {
 		this.fillKey = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F, false);
 		this.transparentKey = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R, false);
 		this.visibilityKey = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.V, false);
+		this.shiftKey = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT, false);
 		
 		this.container = this.scene.add.container(0, 0);
 		this.container.depth = 1000;
@@ -164,8 +166,9 @@ export class TileDrawer extends BaseObject {
 					
 					phaserLayer.putTileAt(tile.id, finalPos.x, finalPos.y, false);
 					
-					// auto tiles
-					Globals.autotileService.drawTile(this.layer, finalPos.x, finalPos.y, tile.id);
+					if (!this.shiftKey.isDown) {
+						Globals.autotileService.drawTile(this.layer, finalPos.x, finalPos.y, tile.id);
+					}
 				}
 			}
 		}
