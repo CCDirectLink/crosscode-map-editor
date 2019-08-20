3b18246b428f923ef49e264aa4b6bf311eca6640
cramerL <vegita321@gmail.com>
2019-08-20 23:15:12 +0200

    removed unused import

--- common/src/controllers/saveFile.ts
+++ common/src/controllers/saveFile.ts
@@ -1,11 +1,8 @@
 import {requireLocal} from '../require';
 
 import * as nodefs from 'fs';
-import * as nodepath from 'path';
 
 const fs: typeof nodefs = requireLocal('fs');
-const path: typeof nodepath = requireLocal('path');
-
 
 export async function saveFile(assetsPath: string, file: { content: string, path: string }) {
 	
