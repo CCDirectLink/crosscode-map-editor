f502db61ab35c06732397088027cdef7e78865cc
cramerL <vegita321@gmail.com>
2019-09-02 22:41:39 +0200

    moved text highlight into own directive

--- webapp/src/app/components/load-map/load-map.component.ts
+++ webapp/src/app/components/load-map/load-map.component.ts
@@ -20,7 +20,7 @@ export class LoadMapComponent {
 	mapsSource = new MatTreeNestedDataSource<VirtualMapNode>();
 
 	root: MapNodeRoot = {name: 'root', displayed: true, children: []}; // The root itself is never displayed. It is used as a datasource for virtualRoot.
-	virtualRoot = new VirtualMapNode(this.root); // To reuse the children filtering. 
+	virtualRoot = new VirtualMapNode(this.root); // To reuse the children filtering.
 	filter = '';
 
 	constructor(
@@ -59,15 +59,6 @@ export class LoadMapComponent {
 
 	close() {
 		return this.sidenav.close();
-	}
-
-	highlight(text: string) {
-		text = this.encodeHTML(text);
-		if (this.filter.length === 0) {
-			return text;
-		}
-
-		return text.split(this.filter).join(`<span class="highlight">${this.filter}</span>`); // Replace all;
 	}
 
 	private displayMaps(paths: string[]) {
@@ -152,9 +143,5 @@ export class LoadMapComponent {
 			child.displayed = true;
 			this.displayChildren(child);
 		}
-	}
-
-	private encodeHTML(text: string) {
-		return text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;') ;
 	}
 }
---
+++ webapp/src/app/shared/highlight.directive.ts
@@ -0,0 +1,35 @@
+import {Directive, ElementRef, Input, OnChanges, SimpleChanges} from '@angular/core';
+
+@Directive({
+	selector: '[appHighlight]'
+})
+export class HighlightDirective implements OnChanges {
+	
+	@Input() highlightText?: string;
+	@Input() highlightMatch?: string;
+	
+	constructor(
+		private element: ElementRef
+	) {
+	
+	}
+	
+	ngOnChanges(changes: SimpleChanges): void {
+		this.element.nativeElement.innerHTML = this.highlight();
+	}
+	
+	highlight() {
+		if (!this.highlightText) {
+			return '';
+		}
+		if (!this.highlightMatch || this.highlightMatch.length === 0) {
+			return this.highlightText;
+		}
+		const pattern = new RegExp(this.highlightMatch, 'i');
+		const match = this.highlightText.match(pattern);
+		if (!match) {
+			return this.highlightText;
+		}
+		return this.highlightText.split(match[0]).join(`<span class="highlight">${match[0]}</span>`);
+	}
+}
