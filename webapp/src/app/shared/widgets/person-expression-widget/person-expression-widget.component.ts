a9cf124a7cedfb27f1d6923904352dc09b42ecc7
cramerL <vegita321@gmail.com>
2019-10-02 12:46:01 +0200

    initialize personExpression object before using it, fix #118

--- webapp/src/app/shared/widgets/person-expression-widget/person-expression-widget.component.ts
+++ webapp/src/app/shared/widgets/person-expression-widget/person-expression-widget.component.ts
@@ -1,14 +1,24 @@
-import {Component} from '@angular/core';
+import {Component, OnChanges, SimpleChanges} from '@angular/core';
 import {AbstractWidget} from '../abstract-widget';
 
 @Component({
 	selector: 'app-person-expression-widget',
-  	templateUrl: './person-expression-widget.component.html',
-  	styleUrls: ['./person-expression-widget.component.scss', '../widget.scss']
+	templateUrl: './person-expression-widget.component.html',
+	styleUrls: ['./person-expression-widget.component.scss', '../widget.scss']
 })
-export class PersonExpressionWidgetComponent extends AbstractWidget {
-
-  	constructor() { 
+export class PersonExpressionWidgetComponent extends AbstractWidget implements OnChanges {
+	
+	constructor() {
 		super();
-  	}
+	}
+	
+	ngOnChanges(changes?: SimpleChanges): void {
+		super.ngOnChanges(changes);
+		if (!this.settings[this.key]) {
+			this.settings[this.key] = {
+				person: '',
+				expression: ''
+			};
+		}
+	}
 }
