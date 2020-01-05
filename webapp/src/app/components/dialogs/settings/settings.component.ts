d5538f55ce8323a9b109c9d6960c0f084aea81c4
ac2pic <ac2pic@gmail.com>
2020-01-05 15:40:35 -0600

    Reverted modifications to settings

--- webapp/src/app/components/dialogs/settings/settings.component.ts
+++ webapp/src/app/components/dialogs/settings/settings.component.ts
@@ -1,10 +1,9 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {OverlayRefControl} from '../../../shared/overlay/overlay-ref-control';
 import {ElectronService} from '../../../services/electron.service';
-import {ModloaderService} from '../../../services/modloader.service';
 import {FormControl} from '@angular/forms';
 import {MatSnackBar} from '@angular/material';
-import {Globals} from '../../../shared/globals';
+
 @Component({
 	selector: 'app-settings',
 	templateUrl: './settings.component.html',
@@ -12,41 +11,24 @@ import {Globals} from '../../../shared/globals';
 })
 export class SettingsComponent implements OnInit {
 	
-	isElectron = Globals.isElectron;
 	folderFormControl = new FormControl();
-	mapContextFormControl = new FormControl();
 	icon = 'help_outline';
 	iconCss = 'icon-undefined';
-	mods: any[] = [{name: 'BASE'}]; 
+	
 	constructor(
 		private ref: OverlayRefControl,
 		private electron: ElectronService,
-		private snackBar: MatSnackBar,
-		private modloader: ModloaderService,
+		private snackBar: MatSnackBar
 	) {
 	}
 	
 	ngOnInit() {
-
-		if (Globals.isElectron) {
-			this.folderFormControl.setValue(this.electron.getAssetsPath());
-			this.folderFormControl.valueChanges.subscribe(() => this.resetIcon());
+		this.folderFormControl.setValue(this.electron.getAssetsPath());
+		this.folderFormControl.valueChanges.subscribe(() => this.resetIcon());
 		
-			if (this.check()) {
-				this.initMods();
-			}
-		} else {
-			this.initMods();
-		}
-
+		this.check();
 	}
 	
-	initMods() {
-		this.mods.splice(1);
-
-		this.modloader.getAllModsAssetsPath().subscribe(mods => this.mods.push(...mods));
-	}
-
 	private resetIcon() {
 		this.icon = 'help_outline';
 		this.iconCss = 'icon-undefined';
@@ -74,33 +56,23 @@ export class SettingsComponent implements OnInit {
 		this.setIcon(valid);
 		if (valid) {
 			this.folderFormControl.setErrors(null);
-			return true;
 		} else {
 			this.folderFormControl.setErrors({
 				invalid: true
 			});
-			return false;
 		}
 	}
 	
 	save() {
-		if (Globals.isElectron) {
-			this.electron.saveAssetsPath(this.folderFormControl.value);
-			const ref = this.snackBar.open('Changing the path requires to restart the editor', 'Restart', {
-				duration: 6000
-			});
-			
-			ref.onAction().subscribe(() => this.electron.relaunch());
-		}
+		this.electron.saveAssetsPath(this.folderFormControl.value);
 		this.close();
+		const ref = this.snackBar.open('Changing the path requires to restart the editor', 'Restart', {
+			duration: 6000
+		});
+		
+		ref.onAction().subscribe(() => this.electron.relaunch());
 	}
 	
-
-	selectMod(index: number) {
-		const mod = this.mods[index];
-		Globals.globalEventsService.changeMapContext.next(mod);
-	}
-
 	close() {
 		this.ref.close();
 	}
