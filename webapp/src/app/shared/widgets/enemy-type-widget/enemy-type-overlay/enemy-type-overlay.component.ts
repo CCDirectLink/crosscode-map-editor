f7384b3f76e6aff5634303de8f3e94d58fbe758f
cramerL <vegita321@gmail.com>
2021-03-26 18:13:09 +0100

    replaces custom input elements with widgets

--- webapp/src/app/shared/widgets/enemy-type-widget/enemy-type-overlay/enemy-type-overlay.component.ts
+++ webapp/src/app/shared/widgets/enemy-type-widget/enemy-type-overlay/enemy-type-overlay.component.ts
@@ -1,5 +1,6 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {AbstractWidget} from '../../abstract-widget';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { AbstractWidget } from '../../abstract-widget';
+import { AttributeValue } from '../../../phaser/entities/cc-entity';
 
 @Component({
 	selector: 'app-enemy-type-overlay',
@@ -8,12 +9,18 @@ import {AbstractWidget} from '../../abstract-widget';
 })
 export class EnemyTypeWidgetOverlayComponent extends AbstractWidget implements OnInit {
 	@Output() exit = new EventEmitter<void>();
-
+	
 	readonly partyOptions = [
 		'PLAYER',
 		'ENEMY',
 		'OTHER'
 	];
+	readonly partyAttributes: AttributeValue = {
+		type: 'string',
+		description: '',
+		withNull: true,
+		options: this.makeOptions(this.partyOptions)
+	};
 	readonly faceOptions = [
 		'NORTH',
 		'EAST',
@@ -24,7 +31,21 @@ export class EnemyTypeWidgetOverlayComponent extends AbstractWidget implements O
 		'SOUTH_WEST',
 		'NORTH_WEST'
 	];
-
+	readonly faceAttributes: AttributeValue = {
+		type: 'string',
+		description: '',
+		withNull: true,
+		options: this.makeOptions(this.faceOptions)
+	};
+	
+	private makeOptions(options: string[]) {
+		const out: { [key: string]: 0 } = {};
+		for (const opt of options) {
+			out[opt] = 0;
+		}
+		return out;
+	}
+	
 	constructor() {
 		super();
 	}
@@ -32,15 +53,15 @@ export class EnemyTypeWidgetOverlayComponent extends AbstractWidget implements O
 	ngOnInit() {
 		super.ngOnInit();
 	}
-
+	
 	close() {
 		this.exit.emit();
 	}
-
+	
 	stringify(arg: unknown): string {
 		return JSON.stringify(arg);
 	}
-
+	
 	parse(arg: string): unknown {
 		try {
 			return JSON.parse(arg);
