1daa481798e64141d74cde43ee13a0951ad44673
cramerL <vegita321@gmail.com>
2021-05-21 17:15:21 +0200

    reset camera position on map load, fixes #127

--- webapp/src/app/shared/phaser/main-scene.ts
+++ webapp/src/app/shared/phaser/main-scene.ts
@@ -8,49 +8,55 @@ import { TileDrawer } from './tilemap/tile-drawer';
 import { EntityManager } from './entities/entity-manager';
 
 export class MainScene extends Phaser.Scene {
-
+	
 	private readonly borderSize = 1;
-
+	
 	private border?: Phaser.GameObjects.Rectangle;
 	private sub?: Subscription;
 	private borderVisible = true;
-
+	
 	constructor() {
-		super({ key: 'main' });
+		super({key: 'main'});
 	}
-
+	
 	preload() {
-
+		
 		this.load.image('pixel', 'assets/pixel.png');
-
+		
 		this.load.json('destructibles.json', 'assets/destructibles.json');
 		this.load.json('destructible-types.json', 'assets/destructible-types.json');
 		this.load.crossOrigin = 'anonymous';
-
+		
 		// this.load.on('progress', (val: number) => console.log(val));
-
+		
 		// this.load.maxParallelDownloads = this.res.images.length;
 		this.load.once('complete', () => Globals.globalEventsService.loadComplete.next());
 	}
-
+	
 	create() {
 		const game = this.game;
 		this.cameras.main.setBackgroundColor('#616161');
 		game.canvas.oncontextmenu = function (e) {
 			e.preventDefault();
 		};
-
+		
 		game.scale.scaleMode = Phaser.Scale.ScaleModes.NONE;
-
+		
 		const entityManager = new EntityManager(this, false);
-
+		
 		const tileMap = new CCMap(game, this, entityManager);
 		Globals.map = tileMap;
-
+		
 		this.sub = Globals.mapLoaderService.map.subscribe((map) => {
 			if (map) {
 				tileMap.loadMap(map);
 				this.rescaleBorder();
+				
+				// reset camera position on map load
+				const cam = this.cameras.main;
+				cam.scrollX = 0;
+				cam.scrollY = 0;
+				cam.zoom = 1;
 			}
 		});
 		Globals.phaserEventsService.updateMapBorder.subscribe(() => this.rescaleBorder());
@@ -60,46 +66,46 @@ export class MainScene extends Phaser.Scene {
 			}
 			this.borderVisible = visible;
 		});
-
+		
 		const pan = new MapPan(this, 'mapPan');
 		this.add.existing(pan);
-
+		
 		const tileDrawer = new TileDrawer(this);
 		this.add.existing(tileDrawer);
-
+		
 		this.add.existing(entityManager);
-
+		
 		Globals.globalEventsService.currentView.subscribe(view => {
 			tileDrawer.setActive(false);
 			entityManager.setActive(false);
 			switch (view) {
-			case EditorView.Layers:
-				tileDrawer.setActive(true);
-				break;
-			case EditorView.Entities:
-				entityManager.setActive(true);
-				break;
+				case EditorView.Layers:
+					tileDrawer.setActive(true);
+					break;
+				case EditorView.Entities:
+					entityManager.setActive(true);
+					break;
 			}
 		});
-
+		
 		Globals.globalEventsService.currentView.next(EditorView.Layers);
 	}
-
+	
 	destroy() {
 		console.log('destroy scene');
 		if (this.sub) {
 			this.sub.unsubscribe();
 		}
 	}
-
+	
 	private rescaleBorder() {
 		const s = Globals.TILE_SIZE;
-
+		
 		if (this.border) {
 			this.border.destroy();
 		}
 		const map = Globals.map;
-
+		
 		this.border = this.add.rectangle(-this.borderSize, -this.borderSize, map.mapWidth * s + this.borderSize * 2, map.mapHeight * s + this.borderSize * 2);
 		this.border.setStrokeStyle(this.borderSize * 2, 0xfc4445, 1);
 		this.border.setOrigin(0, 0);
