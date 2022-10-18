fd77ae80b1c93acd18364e4406cde694454dc11b
cramerL <vegita321@gmail.com>
2022-10-18 18:18:49 +0200

    remove unused uid

--- webapp/src/app/shared/widgets/boolean-widget/boolean-widget.component.ts
+++ webapp/src/app/shared/widgets/boolean-widget/boolean-widget.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {AbstractWidget} from '../abstract-widget';
+import { Component } from '@angular/core';
+import { AbstractWidget } from '../abstract-widget';
 
 @Component({
 	selector: 'app-boolean-widget',
@@ -7,10 +7,4 @@ import {AbstractWidget} from '../abstract-widget';
 	styleUrls: ['./boolean-widget.component.scss', '../widget.scss']
 })
 export class BooleanWidgetComponent extends AbstractWidget {
-	private static counter = 1;
-	uid = BooleanWidgetComponent.counter++;
-	
-	constructor() {
-		super();
-	}
 }
