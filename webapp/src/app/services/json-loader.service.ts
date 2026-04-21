c5abd880f821149054e53a3dbe37d085321927b7
cramerL <vegita321@gmail.com>
2026-04-21 17:43:39 +0200

    cache json promise to avoid repeated requests

--- webapp/src/app/services/json-loader.service.ts
+++ webapp/src/app/services/json-loader.service.ts
@@ -1,22 +1,23 @@
-import { Injectable, inject } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { HttpClientService } from './http-client.service';
 import { HttpClient } from '@angular/common/http';
 import { lastValueFrom } from 'rxjs';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
-	providedIn: 'root'
+	providedIn: 'root',
 })
 export class JsonLoaderService {
 	private http = inject(HttpClientService);
 	private angularHttp = inject(HttpClient);
 	private snackbar = inject(MatSnackBar);
-
+	
 	
 	private readonly initialized?: Promise<void>;
 	private configs = new Map<string, unknown[]>;
 	
 	private cache: Record<string, unknown> = {};
+	private promiseCache: Record<string, Promise<unknown>> = {};
 	
 	constructor() {
 		this.initialized = this.init();
@@ -34,7 +35,7 @@ export class JsonLoaderService {
 				this.snackbar.open(
 					`Failed to parse mod config: ${config.mod}/map-editor/${config.filename}`,
 					'close',
-					{panelClass: 'snackbar-error'}
+					{ panelClass: 'snackbar-error' },
 				);
 				continue;
 			}
@@ -47,24 +48,27 @@ export class JsonLoaderService {
 	async loadJson<T>(file: string): Promise<T[]> {
 		await this.initialized;
 		const json = await lastValueFrom(this.angularHttp.get(`./assets/${file}`));
-		const modJson = (this.configs.get(file) ?? [])  as T[];
+		const modJson = (this.configs.get(file) ?? []) as T[];
 		
 		return [
 			json as T,
-			...modJson
+			...modJson,
 		];
 	}
 	
-	async loadJsonMerged<T>(file: string): Promise<T> {
-		const cached = this.cache[file] as T | undefined;
-		if (cached) {
-			return cached;
+	loadJsonMerged<T>(file: string): Promise<T> {
+		const cachedPromise = this.promiseCache[file] as Promise<T> | undefined;
+		if (cachedPromise) {
+			return cachedPromise;
 		}
-		const jsons = await this.loadJson(file);
-		const base = {} as T;
-		Object.assign(base as any, ...jsons);
-		this.cache[file] = base;
-		return base;
+		const promise = this.loadJson<T>(file).then(jsons => {
+			const base = {} as T;
+			Object.assign(base as any, ...jsons);
+			this.cache[file] = base;
+			return base;
+		});
+		this.promiseCache[file] = promise;
+		return promise;
 	}
 	
 	loadJsonMergedSync<T>(file: string): T {
